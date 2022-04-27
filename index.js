d3817bea3803301825b85fc3c0a3bc3c0a2767c3
Jeena Park <jeenapark15@gmail.com>
2022-04-27 17:54:03 -0400

    commit tree 1fcc6779ad30e7ea04125c3044d0d6213e516a66

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driverName => {
+        const driverLower = driverName.toLowerCase();
+        const nameLower = name.toLowerCase();
+        return driverLower === nameLower;
+    })
+}
+
+function fuzzyMatch(drivers, prefix) {
+    return drivers.filter(driverName => {
+      if (driverName.startsWith(prefix)) {
+          return true;
+      }
+    });
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(driver => {
+        if (driver.name === string) {
+            return true;
+        }
+    })
+}
